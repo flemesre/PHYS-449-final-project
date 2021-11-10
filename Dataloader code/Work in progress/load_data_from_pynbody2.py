28fe77239285055ad114459d7273609d71a54f32
hpcfung <91354621+hpcfung@users.noreply.github.com>
2021-11-10 15:07:26 -0500

    Create load_data_from_pynbody2.py
    2nd ver, works for more than 1 simulation

---
+++ Dataloader code/Work in progress/load_data_from_pynbody2.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pynbody
+
+def IC_file_name(idx):
+    if idx == 2:
+        return 'full-data_reseed'+str(idx)+'_simulation_snapshots_IC_doub_z99_256.gadget3'
+    else:
+        return 'full-data_reseed'+str(idx)+'_simulation_snapshots_IC.gadget3'
+
+if __name__ == '__main__':
+    # change the path to where you store the files on the local machine
+    path = '/mnt/c/Python_projects/data_test/'
+
+    sim_size = 256
+
+    data_list = [1,2]
+
+    for index in data_list:
+        # do I need to use open() for better file handling?
+        f = pynbody.load(path + IC_file_name(index))
+
+        print("Attempting to create den_contrast_"+str(index)+".npy")
+        try:
+            den_contrast = np.load(path+'den_contrast_'+str(index)+'.npy')
+            print("den_contrast_"+str(index)+".npy already created")
+
+        except OSError: #FileNotFoundError
+            rho_m = pynbody.analysis.cosmology.rho_M(f, unit=f["rho"].units)
+            den_contrast = f['rho']/rho_m
+
+            np.save(path+'den_contrast_'+str(index)+'.npy', den_contrast)
+            print('den_contrast_'+str(index)+'.npy created')
+        print()
