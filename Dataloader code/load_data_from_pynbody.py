96ed1efb85e9caecd59ef0de7b0b164986f4cd94
hpcfung <91354621+hpcfung@users.noreply.github.com>
2021-11-10 13:48:41 -0500

    Update load_data_from_pynbody.py
    Removed coords

--- Dataloader code/load_data_from_pynbody.py
+++ Dataloader code/load_data_from_pynbody.py
@@ -10,34 +10,14 @@ if __name__ == '__main__':
     # do I need to use open() for better file handling?
     f = pynbody.load(path + 'full-data_reseed1_simulation_snapshots_IC.gadget3')
 
+    print("Attempting to create den_contrast_1.npy")
     try:
-        print("Attempting to load den_contrast_1.npy")
         den_contrast = np.load(path+'den_contrast_1.npy')
-        print("Loaded density contrast")
+        print("den_contrast_1.npy already created")
 
     except OSError: #FileNotFoundError
-        print('den_contrast_1.npy not found')
-        print('Creating den_contrast_1.npy')
         rho_m = pynbody.analysis.cosmology.rho_M(f, unit=f["rho"].units)
         den_contrast = f['rho']/rho_m
 
         np.save(path+'den_contrast_1.npy', den_contrast)
         print('den_contrast_1.npy created')
-
-    print()
-
-    try:
-        print("Attempting to load coords_1.npy")
-        coords = np.load(path+'coords_1.npy')
-        print("Loaded coordinates of particles")
-        print(coords)
-        print(coords.shape)
-
-    except OSError: #FileNotFoundError
-        print('coords_1.npy not found')
-        print('Creating coords_1.npy')
-        i, j, k = np.unravel_index(f["iord"], (sim_size, sim_size, sim_size))
-        coords = np.column_stack((i, j, k))
-
-        np.save(path+'coords_1.npy', coords)
-        print('coords_1.npy created')
