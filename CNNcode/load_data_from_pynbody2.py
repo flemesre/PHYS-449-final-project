5f53545ff91d9b692fb9bc7a3e466e99ad79659f
hpcfung <91354621+hpcfung@users.noreply.github.com>
2021-11-29 11:43:02 -0500

    Update load_data_from_pynbody2.py
    It can now read .gadget2, hence all 20 sims

--- CNNcode/load_data_from_pynbody2.py
+++ CNNcode/load_data_from_pynbody2.py
@@ -5,15 +5,18 @@ def IC_file_name(idx):
     if idx == 2:
         return 'full-data_reseed'+str(idx)+'_simulation_snapshots_IC_doub_z99_256.gadget3'
     else:
-        return 'full-data_reseed'+str(idx)+'_simulation_snapshots_IC.gadget3'
+        if idx > 5:
+            return 'full-data_reseed' + str(idx) + '_simulation_snapshots_IC.gadget2'
+        else:
+            return 'full-data_reseed' + str(idx) + '_simulation_snapshots_IC.gadget3'
 
 if __name__ == '__main__':
     # change the path to where you store the files on the local machine
-    path = ''
+    path = '' 
 
     sim_size = 256
 
-    data_list = [4,5]
+    data_list = [4,5] # [1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
 
     for index in data_list:
         # do I need to use open() for better file handling?
