9cb22e393367f448431740098fe718e10ca375c7
Tristan Sohrab Fraser <52163923+tsfraser@users.noreply.github.com>
2021-12-05 17:38:24 -0500

    Add files via upload
    Basic plotting scripts from utillss (paper) and my own modifications. TODO: Add a readme

---
+++ truth_plot.py
@@ -0,0 +1,30 @@
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt
+from utilss.plots_for_predictions import plot_violin
+
+def load_truth_pred(prefix):
+    '''
+        INPUT: prefix-- string, the common prefix for .npy data files. Refer to plot_testing.py for the specifics.
+        e.g. CNN_model_iters25000 
+
+        OUTPUT: pred_data -- array of predicted masses
+                truth_data -- array of true masses
+
+
+    '''
+
+    fname_truth = prefix+'true_masses.npy'
+    fname_pred = prefix+'pred_masses.npy'
+
+    pred_data = np.load(fname_pred)
+    truth_data = np.load(fname_truth)
+
+    return pred_data,truth_data
+
+if __name__ == '__main__':
+    prefix = ''
+
+    pred_data,truth_data= load_truth_pred(prefix)
+    f,ax = plot_violin(truth_data,pred_data)
+    plt.show()
