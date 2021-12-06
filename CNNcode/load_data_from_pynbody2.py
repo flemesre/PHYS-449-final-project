89bb3470f5b518dfccb722384a288452360092ad
flemesre <francoislemesre@gmail.com>
2021-12-06 01:56:56 -0500

    code cleanup

--- CNNcode/load_data_from_pynbody2.py
+++ CNNcode/load_data_from_pynbody2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pynbody
 
+
 def IC_file_name(idx):
     if idx == 2:
         return 'full-data_reseed'+str(idx)+'_simulation_snapshots_IC_doub_z99_256.gadget3'
@@ -10,13 +11,14 @@ def IC_file_name(idx):
         else:
             return 'full-data_reseed' + str(idx) + '_simulation_snapshots_IC.gadget3'
 
+
 if __name__ == '__main__':
     # change the path to where you store the files on the local machine
     path = '' 
 
     sim_size = 256
 
-    data_list = [4,5] # [1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+    data_list = [4, 5]  # [1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
 
     for index in data_list:
         # do I need to use open() for better file handling?
@@ -27,7 +29,7 @@ if __name__ == '__main__':
             den_contrast = np.load(path+'den_contrast_'+str(index)+'.npy')
             print("den_contrast_"+str(index)+".npy already created")
 
-        except OSError: #FileNotFoundError
+        except OSError:  # FileNotFoundError
             rho_m = pynbody.analysis.cosmology.rho_M(f, unit=f["rho"].units)
             den_contrast = f['rho']/rho_m
 
