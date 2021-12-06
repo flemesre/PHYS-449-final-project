aae961402dd1db4e806cd463f5bfd462e7e9d43e
Tristan Sohrab Fraser <52163923+tsfraser@users.noreply.github.com>
2021-12-06 14:15:17 -0500

    Documented/cleaned
    TODO: Add relative path to CNN model?

--- CNNcode/PlottingFunctions/single_plot.py
+++ CNNcode/PlottingFunctions/single_plot.py
@@ -2,14 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns
 
-from utilss import plot_violins as pv
-from utilss import plots_for_predictions as pp
-from utilss import predictions_functions as pf
+from utilss.__pycache__ import plot_violins as pv
+from utilss.__pycache__ import plots_for_predictions as pp
+from utilss.__pycache__ import predictions_functions as pf
 
 
-pred_fname = '/Users/tristanfraser/batchsize_16/pred_masses_batch_size_16_CNN_itr35001time1638713522.pt.npy'
+pred_fname = '/Users/tristanfraser/skip_connection/pred_masses_skip_CNN_at25001_itr35001time1638785933.pt.npy'
+#pred_masses_skip_CNN_itr35001time1638789683.pt.npy'#/11_sims/stoppingat_5001/pred_masses_11_sims_CNN_at5001_itr35001time1638740279.pt.npy'#11_sims/stoppingat_5001/pred_masses_11_sims_CNN_at5001_itr35001time1638740279.pt.npy'#pred_masses_batch_size_16_CNN_itr35001time1638713522.pt.npy'
 #'/Users/tristanfraser/11_sims/all_35001_iterations/pred_masses_11_sims_CNN_itr35001time1638751400.pt.npy'
-true_fname = '/Users/tristanfraser/batchsize_16/true_masses_batch_size_16_CNN_itr35001time1638713522.pt.npy'
+true_fname = '/Users/tristanfraser/skip_connection/true_masses_skip_CNN_at25001_itr35001time1638785933.pt.npy'
+#true_masses_skip_CNN_itr35001time1638789683.pt.npy'#'#batchsize_8/11_sims/stoppingat_5001/true_masses_11_sims_CNN_at5001_itr35001time1638740279.pt.npy'#true_masses_batch_size_16_CNN_itr35001time1638713522.pt.npy'
 # '/Users/tristanfraser/11_sims/all_35001_iterations/true_masses_11_sims_CNN_itr35001time1638751400.pt.npy'
 
 
@@ -20,9 +22,9 @@ pred = 1.2*(np.load(pred_fname)+1)+11
 
 f,ax =pv.plot_violin(truth, pred, bins_violin=None,
                     return_stats=None, box=False, alpha=0.5, vert=True, col="C0", figsize=(8, 8))
-ax.set_title('Paper CNN -- 35000 iterations - batch size = 16')
-plt.savefig('/Users/tristanfraser/batchsize_16/Violins_16bs.png')
+ax.set_title('Skip CNN -- 25001 iterations')
+plt.savefig('/Users/tristanfraser/skip_connection/skip25001Violins.png')
 
 f2, a, m = pp.plot_histogram_predictions(pred, truth, radius_bins=False, particle_ids=None, errorbars=False,
                                          label="Raw density", color="C0")
-plt.savefig('/Users/tristanfraser/batchsize_16/residuals_batchsize16.png')
+plt.savefig('/Users/tristanfraser/skip_connection/skip25001residuals.png')
